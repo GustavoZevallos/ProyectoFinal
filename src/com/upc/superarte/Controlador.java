9613df7edddf37768abccf21b633ff29caad9afb
Gustavo zevallos <89364452+GustavoZevallos@users.noreply.github.com>
2021-10-01 02:56:34 -0500

    -recepcionista +Factoria y controlador

--- src/com/upc/superarte/Controlador.java
+++ src/com/upc/superarte/Controlador.java
@@ -21,7 +21,7 @@ public class Controlador {
     }
 
     public void asignarSesion(Paciente paciente, Psicologo psicologo, String... arg){
-        clinica.asignarSesion(paciente,psicologo,arg[2],arg[3],arg[4],arg[5],arg[6]);
+        clinica.asignarSesion(paciente,psicologo,arg[0],arg[1],arg[2],arg[3],arg[4]);
     }
 
     public Clinica getClinica() {
